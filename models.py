84a6b84a85599c8332af947849a2fc4cf926b8f2
Mara Fennema <maradfennema@gmail.com>
2020-06-11 18:05:42 +0200

    now the correct file for lstm

--- models.py
+++ models.py
@@ -1,40 +1,119 @@
 import torch
 import torch.nn as nn
 from torch.autograd import Variable
+from torch.nn import functional as F
+
+"""
+Blog post:
+Taming LSTMs: Variable-sized mini-batches and why PyTorch is good for your health:
+https://medium.com/@_willfalcon/taming-lstms-variable-sized-mini-batches-and-why-pytorch-is-good-for-your-health-61d35642972e
+"""
+
 
 class myLSTM(nn.Module):
-    def __init__(self, in_features, out_features, batch_size, num_layers):
-        super(myLSTM, self).__init__()
-        self.lstm = nn.LSTM(in_features, out_features, num_layers, batch_first=True)
+    def __init__(self, nb_layers, nb_lstm_units=100, embedding_dim=3, batch_size=3, indiv2idx, char2idx):
+        self.vocab = char2idx
+        self.tags = indiv2idx
+
+        self.nb_layers = nb_layers
+        self.nb_lstm_units = nb_lstm_units
+        self.embedding_dim = embedding_dim
         self.batch_size = batch_size
 
+        # don't count the padding tag for the classifier output
+        self.nb_tags = len(self.tags) - 1
+
+        # when the model is bidirectional we double the output dimension
+        self.lstm
+
+        # build actual NN
+        self.__build_model()
+
+    def __build_model(self):
+        # build embedding layer first
+        nb_vocab_words = len(self.vocab)
+
+        # whenever the embedding sees the padding index it'll make the whole vector zeros
+        padding_idx = self.vocab['0']
+        self.word_embedding = nn.Embedding(
+            num_embeddings=nb_vocab_words,
+            embedding_dim=self.embedding_dim,
+            padding_idx=padding_idx
+        )
+
+        # design LSTM
+        self.lstm = nn.LSTM(
+            input_size=self.embedding_dim,
+            hidden_size=self.nb_lstm_units,
+            num_layers=self.nb_lstm_layers,
+            batch_first=True,
+        )
+
+        # output layer which projects back to tag space
+        self.hidden_to_tag = nn.Linear(self.nb_lstm_units, self.nb_tags)
+
+    def init_hidden(self):
+        # the weights are of the form (nb_layers, batch_size, nb_lstm_units)
+        hidden_a = torch.randn(self.hparams.nb_lstm_layers, self.batch_size, self.nb_lstm_units)
+        hidden_b = torch.randn(self.hparams.nb_lstm_layers, self.batch_size, self.nb_lstm_units)
+
+        if self.hparams.on_gpu:
+            hidden_a = hidden_a.cuda()
+            hidden_b = hidden_b.cuda()
+
+        hidden_a = Variable(hidden_a)
+        hidden_b = Variable(hidden_b)
     
-    def forward(self, inputs, returnHidden=False):
-        out, hidden = self.lstm(inputs.view(inputs.size(0), -1, 1))
-        if returnHidden:
-            return out, hidden
-        else:
-            return out
+        def forward(self, X, X_lengths):
+        # reset the LSTM hidden state. Must be done before you run a new batch. Otherwise the LSTM will treat
+        # a new batch as a continuation of a sequence
+        self.hidden = self.init_hidden()
 
+        batch_size, seq_len, _ = X.size()
 
+        # ---------------------
+        # 1. embed the input
+        # Dim transformation: (batch_size, seq_len, 1) -> (batch_size, seq_len, embedding_dim)
+        X = self.word_embedding(X)
 
+        # ---------------------
+        # 2. Run through RNN
+        # TRICK 2 ********************************
+        # Dim transformation: (batch_size, seq_len, embedding_dim) -> (batch_size, seq_len, nb_lstm_units)
 
-class RNN(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
-        super(RNN, self).__init__()
+        # pack_padded_sequence so that padded items in the sequence won't be shown to the LSTM
+        X = torch.nn.utils.rnn.pack_padded_sequence(x, X_lengths, batch_first=True)
 
-        self.hidden_size = hidden_size
+        # now run through LSTM
+        X, self.hidden = self.lstm(X, self.hidden)
 
-        self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
-        self.i2o = nn.Linear(input_size + hidden_size, output_size)
-        self.softmax = nn.LogSoftmax(dim=1)
+        # undo the packing operation
+        X, _ = torch.nn.utils.rnn.pad_packed_sequence(X, batch_first=True)
 
-    def forward(self, input, hidden):
-        combined = torch.cat((input, hidden), 1)
-        hidden = self.i2h(combined)
-        output = self.i2o(combined)
-        output = self.softmax(output)
-        return output, hidden
+        # ---------------------
+        # 3. Project to tag space
+        # Dim transformation: (batch_size, seq_len, nb_lstm_units) -> (batch_size * seq_len, nb_lstm_units)
 
-    def initHidden(self):
-        return torch.zeros(1, self.hidden_size)
+        # this one is a bit tricky as well. First we need to reshape the data so it goes into the linear layer
+        X = X.contiguous()
+        X = X.view(-1, X.shape[2])
+
+        # run through actual linear layer
+        X = self.hidden_to_tag(X)
+
+        # ---------------------
+        # 4. Create softmax activations bc we're doing classification
+        # Dim transformation: (batch_size * seq_len, nb_lstm_units) -> (batch_size, seq_len, nb_tags)
+        X = F.log_softmax(X, dim=1)
+
+        # I like to reshape for mental sanity so we're back to (batch_size, seq_len, nb_tags)
+        X = X.view(batch_size, seq_len, self.nb_tags)
+
+        Y_hat = X
+        return Y_hat
+
+    
+    def loss(self, y_hat, y):
+        criterion = torch.nn.CrossEntropyLoss(ignore_index=0)
+        ce_loss = criterion(y_hat, y.argmax())
+        return ce_loss
